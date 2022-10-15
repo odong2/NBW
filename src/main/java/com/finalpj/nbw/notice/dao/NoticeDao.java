f1edfbb4db462dd0ba491d2bc021f576d5c853ff
odong2 <mjey54@naver.com>
2022-10-15 23:11:37 +0900

    Feat: NoticeService 테스트 중

--- src/main/java/com/finalpj/nbw/notice/dao/NoticeDao.java
+++ src/main/java/com/finalpj/nbw/notice/dao/NoticeDao.java
@@ -4,19 +4,20 @@ import com.finalpj.nbw.notice.domain.Notice;
 import org.apache.ibatis.session.SqlSession;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface NoticeDao {
-    int insertNotice(Notice noticeDto) throws Exception; // 공지사항 등록
+    int insertNotice(Notice noticeDto) throws Exception; // 공지글 등록
 
-    int updateNotice(Notice noticeDto) throws Exception; // 공지사항 수정
+    int updateNotice(Notice noticeDto) throws Exception; // 공지글 수정
 
-    int deleteNotice(Notice noticeDto) throws Exception; // 공지사항 한 건 삭제
+    int deleteNotice(Integer nt_no) throws Exception; // 공지글 한 건 삭제
 
-    int deleteAllNotice() throws Exception; // 공지사항 전체 삭제
+    int deleteNoticeList() throws Exception; // 공지글 전체 삭제
 
-    Notice selectNoticeOne(); // Notice 한건 조회
+    Notice selectNotice(Integer nt_no) throws Exception; // 공지글 한건 조회
 
-    ArrayList<Notice> selectNoticeList(); // Notice 전체 조회
+    List<Notice> selectNoticeList() throws Exception; // 공지글 전체 조회
 
 
 }
--- src/main/java/com/finalpj/nbw/notice/repository/NoticeDaoImpl.java
+++ src/main/java/com/finalpj/nbw/notice/repository/NoticeDaoImpl.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.List;
+
 @Log4j
 @Repository
 public class NoticeDaoImpl implements NoticeDao {
@@ -15,34 +17,33 @@ public class NoticeDaoImpl implements NoticeDao {
     @Autowired
     private SqlSession sqlSession;
 
-
+    /* ========================== 공지글 등록 ============================*/
     @Override
     public int insertNotice(Notice noticeDto) throws Exception {
-        return sqlSession.insert("insert", noticeDto);
+        return sqlSession.insert("insertNotice", noticeDto);
     }
-
+    /* ========================== 공지글 수정 ============================*/
     @Override
     public int updateNotice(Notice noticeDto) throws Exception {
-        return 0;
+        return sqlSession.update("updateNotice", noticeDto);
+    }
+    /* ========================== 공지글 한 건 삭제 ============================*/
+    @Override
+    public int deleteNotice(Integer nt_no) throws Exception {
+        return sqlSession.delete("deleteNotice", nt_no);
+    }
+    /* ========================== 공지글 전체 삭제 ============================*/
+    @Override
+    public int deleteNoticeList() throws Exception {
+        return sqlSession.delete("deleteNoticeList");
     }
 
     @Override
-    public int deleteNotice(Notice noticeDto) throws Exception {
-        return 0;
+    public Notice selectNotice(Integer nt_no) throws Exception{
+        return sqlSession.selectOne("selectNotice", nt_no);
     }
-
     @Override
-    public int deleteAllNotice() throws Exception {
-        return 0;
-    }
-
-    @Override
-    public Notice selectNoticeOne() {
-        return null;
-    }
-
-    @Override
-    public ArrayList<Notice> selectNoticeList() {
-        return null;
+    public List<Notice> selectNoticeList() throws Exception{
+        return sqlSession.selectList("selectNoticeList");
     }
 }
