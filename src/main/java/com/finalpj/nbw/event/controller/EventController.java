62165657e146a895609dccec51dedb8e54816982
dongcarry96 <fhakwpak6@naver.com>
2022-10-17 12:34:38 +0900

    Design: 이벤트리스트 페이지 추가

--- src/main/java/com/finalpj/nbw/event/controller/EventController.java
+++ src/main/java/com/finalpj/nbw/event/controller/EventController.java
@@ -1,4 +1,18 @@
 package com.finalpj.nbw.event.controller;
 
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j
+@RequestMapping("/event/*")
+@Controller
 public class EventController {
+
+    @GetMapping("eventTest")
+    public String eventTest() {
+        log.info("테스트");
+        return "eventList";
+    }
 }
