ac3141cd4c5869dc7b9df5f311d00e4f2b827acd
dongcarry96 <fhakwpak6@naver.com>
2022-10-18 11:24:36 +0900

    테스트

--- src/main/java/com/finalpj/nbw/event/controller/EventController.java
+++ src/main/java/com/finalpj/nbw/event/controller/EventController.java
@@ -1,4 +1,21 @@
 package com.finalpj.nbw.event.controller;
 
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j
+@RequestMapping("/event/*")
+@Controller
 public class EventController {
+
+    @GetMapping("/eventList")
+    public String eventList() {
+        return "eventList";
+    }
+    @GetMapping("/eventDetail")
+    public String eventDetail() {
+        return "eventDetail";
+    }
 }
