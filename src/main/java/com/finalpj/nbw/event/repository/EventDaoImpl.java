8fba045010d19dadf3dcab4eb4362af66aefeedb
dongcarry96 <fhakwpak6@naver.com>
2022-10-20 17:03:58 +0900

    Feat: 이벤트 dao 수정중

--- src/main/java/com/finalpj/nbw/event/repository/EventDaoImpl.java
+++ src/main/java/com/finalpj/nbw/event/repository/EventDaoImpl.java
@@ -1,11 +1,17 @@
 package com.finalpj.nbw.event.repository;
 
 import com.finalpj.nbw.event.dao.EventDao;
+import com.finalpj.nbw.event.domain.Event;
+import com.finalpj.nbw.notice.domain.Notice;
+import com.finalpj.nbw.notice.domain.SearchCondition;
 import lombok.extern.log4j.Log4j;
 import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
 
 @Log4j
 @Repository
@@ -13,8 +19,72 @@ public class EventDaoImpl implements EventDao {
 
     private SqlSession sqlSession;
 
-    public EventDaoImpl()
+    public EventDaoImpl(SqlSession sqlSession){this.sqlSession = sqlSession;}
 
+    /* 이벤트 등록 */
     @Override
-    public
+    public int insertEvent(Event eventDto) throws Exception {
+        return sqlSession.insert("insertEvent", eventDto);
+    }
+
+    /* 이벤트 수정 */
+    @Override
+    public int updateEvent(Event eventDto) throws Exception {
+        return sqlSession.update("updateEvent", eventDto);
+    }
+
+    /* 이벤트글 한건 삭제 */
+    @Override
+    public int deleteEvent(Integer ev_no) throws Exception {
+        return  sqlSession.delete("deleteEvent", ev_no);
+    }
+
+    /* 이벤트글 전체 삭제 */
+    @Override
+    public int deleteEventList() throws Exception {
+        return sqlSession.delete("deleteEventList");
+    }
+
+    /* 이벤트글 한 건 조회 */
+    @Override
+    public Event selectEvent(Integer ev_no) throws Exception{
+        return sqlSession.selectOne("selectEvent", ev_no);
+    }
+
+    /* 이벤트글 전체 조회 */
+    @Override
+    public List<Event> selectEventList() throws Exception{
+        return sqlSession.selectList("selectEventList");
+    }
+
+    /* 이벤트글 조회수 증가 */
+    @Override
+    public int updateViewCnt(Integer ev_no) throws Exception{
+        return sqlSession.update("updateViewCnt",ev_no);
+    }
+
+    /* 공지글 전체 개수 */
+    @Override
+    public int selectEventCnt() throws Exception{
+        return sqlSession.selectOne("selectEventCnt");
+    }
+
+    /* 페이지 게시물 조회 */
+    @Override
+    public List<Event> selectEventPage(Map map) throws Exception{
+        return sqlSession.selectList("selectEventPage", map);
+    }
+
+    /* 조건검색 페이징 처리하여 조회 */
+    @Override
+    public List<Event> selectSearchPage(SearchCondition sc) throws Exception{
+        return sqlSession.selectList("selectSearchPage", sc);
+    }
+
+    /* 조건검색 결과 게시물 개수 */
+    @Override
+    public int selectSearchCnt(SearchCondition sc) throws Exception{
+        return sqlSession.selectOne("selectSearchCnt", sc);
+    }
+
 }
--- src/main/java/com/finalpj/nbw/event/service/EventService.java
+++ src/main/java/com/finalpj/nbw/event/service/EventService.java
@@ -1,4 +1,68 @@
 package com.finalpj.nbw.event.service;
 
+import com.finalpj.nbw.event.dao.EventDao;
+import com.finalpj.nbw.event.domain.Event;
+import com.finalpj.nbw.notice.domain.Notice;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+@Log4j
+@Service
 public class EventService {
+
+    private final EventDao eventDao;
+
+    public EventService(EventDao eventDao) { this.eventDao=eventDao;}
+
+    /* 공지글 입력 */
+    @Transactional(rollbackFor = Exception.class)
+    public int registEvent(Event eventDto) throws Exception{
+        return eventDao.insertEvent(eventDto);
+    }
+    /* 공지글 수정 */
+    @Transactional(rollbackFor = Exception.class)
+    public int modifyEvent(Event EventDto) throws Exception{
+        return eventDao.updateEvent(eventDto);
+    }
+    /* 공지글 한 건 삭제 */
+    @Transactional(rollbackFor = Exception.class)
+    public int removeNotice(Integer ev_no) throws Exception{
+        return EventDao.deleteEvent(ev_no);
+    }
+    /* 공지글 전체 삭제 */
+    @Transactional(rollbackFor = Exception.class)
+    public int removeAllEvent() throws Exception{
+        return EventDao.deleteEventList();
+    }
+    /* 공지글 한 건 조회  + 조회수 증가 */
+    @Transactional(readOnly = true)
+    public Event getEvent(Integer ev_no) throws Exception{
+        Event EventDto = EventDao.selectEvent(ev_no);
+        EventDao.updateViewCnt(nt_no);
+
+        return noticeDto;
+    }
+    /* 공지글 전체 조회 */
+    @Transactional(readOnly = true)
+    public List<Notice> getAllEvent() throws Exception{
+        return EventDao.selectEventList();
+    }
+
+    /* 공지글 조회수 증가 */
+    @Transactional(rollbackFor = Exception.class)
+    public int increaseViewCnt(Integer ev_no) throws Exception{
+        return EventDao.updateViewCnt(ev_no);
+    }
+    public int getEventTotalCnt() throws Exception{
+        return EventDao.selectEventCnt();
+    }
+
+    /* 페이지 게시물 조회 */
+    public List<Notice> getPageEventList(Map map) throws Exception{
+        return EventDao.selectEventPage(map);
+    }
 }
