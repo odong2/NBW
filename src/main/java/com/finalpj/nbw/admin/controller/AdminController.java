46e2d00cc57a2ffe071db99e076dfddbd852889f
jongGuk <qorwhdr@naver.com>
2022-10-21 10:41:06 +0900

    Design: admin 홈페이지 화면 추가

---
+++ src/main/java/com/finalpj/nbw/admin/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.finalpj.nbw.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+	
+	@GetMapping("/admin")
+	public String main() {
+		
+		return "admin/home";
+	}
+	
+}
