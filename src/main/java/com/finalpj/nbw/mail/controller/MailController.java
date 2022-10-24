c5faf85be7a24425c43237081eff991df2039032
정콩순 <ninxtxn@gmail.com>
2022-10-24 20:13:36 +0900

    Feat: 메일 컨트롤러(인증 메일 발송) 추가

--- src/main/java/com/finalpj/nbw/mail/controller/MailController.java
+++ src/main/java/com/finalpj/nbw/mail/controller/MailController.java
@@ -1,17 +1,23 @@
 package com.finalpj.nbw.mail.controller;
 
 import com.finalpj.nbw.mail.service.MailService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.util.Random;
 
 @Controller
+@Slf4j
 @EnableAsync // 메서드를 호출할 경우 비동기로 동작하게 하는 어노테이션
 @RequestMapping("/mail/")
 public class MailController {
@@ -19,16 +25,35 @@ public class MailController {
     @Autowired
     private MailService mailService;
 
-    @GetMapping("send/")
+    /* ==================== 인증 메일 전송 ===================== */
+    @GetMapping (value="mailCheck")
+    @ResponseBody
+    public String mailCheckGET(String email){
 
-    public void sendSimpleMail(HttpServletRequest request, HttpServletResponse response) throws Exception{
-        request.setCharacterEncoding("utf-8");
-        response.setContentType("text/html; charset=utf-8");
-        PrintWriter out = response.getWriter();
-        /* (1) mailService 의 sendMail() 메서드로 관련 값 {*주소*, *제목*, *내용*} */
-        mailService.sendMail("ninxtxn@gmail.com", "테스트 메일입니다.", "안녕하세요? 테스트 메일입니다.");
-        /* (2) mail-context.xml 에 설정한 메일 주소로 내용을 보낸다. */
-        //mailService.sendPreConfiguredMail("테스트 메일입니다.");
-        out.print("메일을 보냈습니다.");
+        /* 뷰(View)로부터 넘어온 데이터 확인 */
+        log.info("이메일 데이터 전송 확인");
+        log.info("메일 주소 ===> " + email);
+
+        /* 인증 코드 생성 */
+        Random random = new Random();
+        int ranCode = random.nextInt(888888) + 111111;
+
+        log.info("생성된 코드는 ===>" +ranCode);
+
+        try {
+            /* 이메일을 보낸다.  -> Service 의 send 에 넘겨줄 값을 변수에 담는다. */
+            String toMail = email;
+            String subject = "NBW 인증메일 발송입니다.";
+            /* 내용에는 생성한 난수를 담는다. */
+            String body = "인증 코드는 "+ ranCode + "입니다.";
+            /* 서비스 층에서 메일을 전송한다. */
+            mailService.sendMail(email, subject, body);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /* 뷰(View)로 전송할 코드를 String 형식으로 형변환 후 반환해서 ajax 로 뷰로 다시 반환한다. */
+        return Integer.toString(ranCode);
     }
+
 }
