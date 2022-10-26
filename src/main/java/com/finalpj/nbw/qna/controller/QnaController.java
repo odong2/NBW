bafef49004b7dfb8a49ab5501830572ed3dff343
dongcarry96 <fhakwpak6@naver.com>
2022-10-26 16:18:23 +0900

    Feat: QNA컨트롤러 수정

--- src/main/java/com/finalpj/nbw/qna/controller/QnaController.java
+++ src/main/java/com/finalpj/nbw/qna/controller/QnaController.java
@@ -2,16 +2,10 @@ package com.finalpj.nbw.qna.controller;
 
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log4j
-@RequestMapping("/error/*")
+@RequestMapping("/event/*")
 @Controller
 public class QnaController {
-
-    @GetMapping("error")
-    public String errorTest() {
-        return "/error/error";
-    }
 }
