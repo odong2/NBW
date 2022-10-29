c5cbbbdfd4fc969de86f7a0c131c29269e03d48a
dongcarry96 <fhakwpak6@naver.com>
2022-10-29 22:33:12 +0900

    Feat: 이벤트 도메인 수정중

--- src/main/java/com/finalpj/nbw/event/domain/Event.java
+++ src/main/java/com/finalpj/nbw/event/domain/Event.java
@@ -3,29 +3,33 @@ package com.finalpj.nbw.event.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Event {
-    Integer ev_no; //
-    String ev_title; // 행사이름
-    String ev_content; // 행사설명(글)
-    Date ev_cdate; // 게시글 등록날짜
-    String ev_file; // 첨부파일
-    Integer ev_hit; // 조회수
-    String ev_start; // 행사 시작일
-    String ev_end; // 행사 종료일
-    Integer ev_people; // 모집 정원
-    String ev_status; // 상태(참여중/참여가능)
-    Integer ev_nowpeople; // 남은정원
-    String ev_target; // 이벤트 대상
-    String ev_place; // 이벤트 장소
-    String ev_img; // 행사 포스터
-    String ev_today; // 교육기간
-    String ev_phone; // 문의번호
+    private Integer ev_no; //
+    private String ev_title; // 행사이름
+    private String ev_content; // 행사설명(글)
+    private Date ev_cdate; // 게시글 등록날짜
+    private String ev_file; // 첨부파일
+    private Integer ev_hit; // 조회수
+    private String ev_start; // 행사 시작일
+    private String ev_end; // 행사 종료일
+    private Integer ev_people; // 모집 정원
+    private String ev_status; // 상태(참여중/참여가능)
+    private Integer ev_nowpeople; // 남은정원
+    private String ev_target; // 이벤트 대상
+    private String ev_place; // 이벤트 장소
+    private String ev_img; // 행사 포스터
+    private String ev_today; // 교육기간
+    private String ev_phone; // 문의번호
+
+    private Integer ev_cocnt; // 댓글카운트
 
     public Event (Integer ev_no, String ev_title, String ev_content, String ev_file,
                   String ev_start, String ev_end, Integer ev_people, String ev_target,
