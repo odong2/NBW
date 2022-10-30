7a230e9444bd993b979214cf837c0135ac1e31bb
dongcarry96 <fhakwpak6@naver.com>
2022-10-30 14:30:04 +0900

    Feat: 이벤트 도메인 수정중

--- src/main/java/com/finalpj/nbw/event/domain/Event.java
+++ src/main/java/com/finalpj/nbw/event/domain/Event.java
@@ -24,6 +24,7 @@ public class Event {
     private String ev_status; // 상태(참여중/참여가능)
     private Integer ev_nowpeople; // 남은정원
     private String ev_target; // 이벤트 대상
+    private String ev_time; // 이벤트 시간
     private String ev_place; // 이벤트 장소
     private String ev_img; // 행사 포스터
     private String ev_today; // 교육기간
@@ -33,7 +34,7 @@ public class Event {
 
     public Event (Integer ev_no, String ev_title, String ev_content, String ev_file,
                   String ev_start, String ev_end, Integer ev_people, String ev_target,
-                  String ev_place, String ev_img, String ev_today, String ev_phone) {
+                  String ev_place, String ev_img, String ev_today, String ev_phone, String ev_time) {
         this.ev_no = ev_no;
         this.ev_title = ev_title;
         this.ev_content = ev_content;
@@ -46,6 +47,7 @@ public class Event {
         this.ev_img = ev_img;
         this.ev_today = ev_today;
         this.ev_phone = ev_phone;
+        this.ev_time = ev_time;
     }
 
 
