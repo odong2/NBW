c4a7f1fdb6dfd9a240eb320d2e4e6e611844deaa
odong2 <mjey54@naver.com>
2022-10-30 23:54:11 +0900

    Feat: payment에서 사용하는 Dto 생성

--- src/main/java/com/finalpj/nbw/payment/domain/CartProduct.java
+++ src/main/java/com/finalpj/nbw/payment/domain/CartProduct.java
@@ -1,9 +1,17 @@
 package com.finalpj.nbw.payment.domain;
 
 import com.finalpj.nbw.product.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class CartProduct {
     private String p_no;
     private String p_title;
@@ -11,8 +19,5 @@ public class CartProduct {
     private String p_img;
     private String p_category;
     private int mcart_count;
-    private String mem_id;
-    private Product product;
 
-    private List<CartProduct> cartProductList;
 }
--- src/main/java/com/finalpj/nbw/payment/domain/MultiProduct.java
+++ src/main/java/com/finalpj/nbw/payment/domain/MultiProduct.java
@@ -1,2 +1,14 @@
-package com.finalpj.nbw.payment.domain;public class MultiProduct {
+package com.finalpj.nbw.payment.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MultiProduct {
+    private List<CartProduct> cartProducts;
 }
