bd51b391a895f85f1f1dde0e3d04679c63bc66f3
dongcarry96 <fhakwpak6@naver.com>
2022-10-30 14:29:01 +0900

    Feat: 이벤트 컨트롤러 수정중

--- src/main/java/com/finalpj/nbw/event/controller/EventController.java
+++ src/main/java/com/finalpj/nbw/event/controller/EventController.java
@@ -1,9 +1,13 @@
 package com.finalpj.nbw.event.controller;
 
 import com.finalpj.nbw.event.domain.Event;
-import com.finalpj.nbw.event.service.EventService;
+import com.finalpj.nbw.event.domain.PageHandler;
 import com.finalpj.nbw.event.domain.SearchCondition;
+import com.finalpj.nbw.event.service.EventService;
+import com.finalpj.nbw.member.controller.MemberController;
 import lombok.extern.log4j.Log4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,27 +23,36 @@ public class EventController {
 
     EventService eventService;
 
+    Logger logger = LoggerFactory.getLogger(EventController.class);
+
     public EventController(EventService eventService){
         this.eventService = eventService;
     }
 
-    @GetMapping("/detail")
-    public String eventDetail(){
-        return "/event/eventDetail";
-    }
+//    @GetMapping("/detail")
+//    public String eventDetail(){
+//        return "/event/eventDetail";
+//    }
+//    @GetMapping("/list")
+//    public String eventList(){
+//        return "/event/eventList";
+//    }
+
     @GetMapping("/list")
-    public String eventList(){
+    public String eventList(Model model) throws Exception {
+        logger.info("컨트롤러 이벤트 리스트 호출 성공");
+        model.addAttribute("eventListAll", eventService.eventList());
+//        logger.info("Event컨트롤러");
+        logger.info("이벤트 호출 성공"+eventService.eventList().toString());
         return "/event/eventList";
     }
 
-    // 공지글 목록 페이지 요청
-
-    @GetMapping("read")
+    @GetMapping("/detail")
     public String read(Integer ev_no, SearchCondition sc, Model m){
         try {
-            Event eventDto = eventService.getEvent(ev_no);
+            Event eventDto = eventService.selectList(ev_no);
             m.addAttribute("eventDto", eventDto);
-            m.addAttribute("SearchCondition", sc);
+//            m.addAttribute("SearchCondition", sc);
             log.info("eventDto = " + eventDto);
         } catch (Exception e) {
             e.printStackTrace();
