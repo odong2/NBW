8b6bed6c907d64722bb456ff156c07e5a730235f
dongcarry96 <fhakwpak6@naver.com>
2022-10-30 14:29:30 +0900

    Feat: 이벤트다오 수정중

--- src/main/java/com/finalpj/nbw/event/dao/EventDao.java
+++ src/main/java/com/finalpj/nbw/event/dao/EventDao.java
@@ -1,7 +1,7 @@
 package com.finalpj.nbw.event.dao;
 
 import com.finalpj.nbw.event.domain.Event;
-import com.finalpj.nbw.event.domain.SearchCondition;
+import com.finalpj.nbw.notice.domain.SearchCondition;
 
 import java.util.List;
 import java.util.Map;
@@ -17,7 +17,13 @@ public interface EventDao {
 
     Event select(Integer ev_no) throws Exception; // 공지글 한건 조회
 
-    List<Event> selectList() throws Exception; // 공지글 전체 조회
+
+//    Event selectList(Integer ev_no) throws Exception;
+
+    Event selectList(Integer ev_no) throws Exception;
+
+    //공지글 전체 조회
+    List<Event> eventList() throws Exception;
 
     int updateViewCnt(Integer ev_no) throws Exception; // 조회수 증가
 
@@ -26,9 +32,10 @@ public interface EventDao {
     List<Event> selectPage(Map map) throws Exception; // 공지글 페이지 조회
 
 
-    List<Event> selectSearchPage(SearchCondition sc) throws Exception; // 조건 검색 페이징 처리하여 조회
 
-    int selectSearchCnt(SearchCondition sc) throws Exception; // 조건검색 결과 게시물 개수
+//    List<Event> selectSearchPage(SearchCondition sc) throws Exception; // 조건 검색 페이징 처리하여 조회
+
+//    int selectSearchCnt(SearchCondition sc) throws Exception; // 조건검색 결과 게시물 개수
 
     int updateCommentCnt(Integer ev_no, int ev_cocnt) throws Exception; // 댓글수 업데이트
 
