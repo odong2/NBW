d1ca888daf6ec068a49446f9a0e3146540a2961b
odong2 <mjey54@naver.com>
2022-11-01 02:53:42 +0900

    Feat: Member 쿠폰 조회기능 완성

--- src/main/java/com/finalpj/nbw/coupon/dao/CouponDao.java
+++ src/main/java/com/finalpj/nbw/coupon/dao/CouponDao.java
@@ -1,5 +1,13 @@
 package com.finalpj.nbw.coupon.dao;
 
+import com.finalpj.nbw.coupon.domain.Coupon;
+import com.finalpj.nbw.notice.domain.Notice;
+
+import java.util.List;
+
 public interface CouponDao {
+
+    int selectIsCoupon(String mem_id) throws Exception; // 쿠폰 존재 여부
+    List<Coupon> selectCouponList(String mem_id) throws Exception; // 멤버 쿠폰 가져오기
 
 }
--- src/main/java/com/finalpj/nbw/coupon/service/CouponService.java
+++ src/main/java/com/finalpj/nbw/coupon/service/CouponService.java
@@ -1,5 +1,35 @@
 package com.finalpj.nbw.coupon.service;
 
+import com.finalpj.nbw.coupon.dao.CouponDao;
+import com.finalpj.nbw.coupon.domain.Coupon;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Log4j
+@Service
 public class CouponService {
+    private final CouponDao couponDao;
+    public CouponService(CouponDao couponDao){
+        this.couponDao = couponDao;
+    }
+
+    /**************************** 회원 쿠폰정보 조회 *****************************/
+    public List<Coupon> getCouponList(String mem_id) throws Exception{
+
+        List<Coupon> couponList = null;
+        int isCoupon = couponDao.selectIsCoupon(mem_id);
+        log.info("쿠폰 존재 여부 ====> " + isCoupon);
+
+        if(isCoupon > 0){
+            log.info("쿠폰이 존재함");
+            couponList = couponDao.selectCouponList(mem_id);
+        }else {
+            log.info("쿠폰이 존재하지 않음");
+            couponList = new ArrayList();
+        }
+        return couponList;
+    }
 
 }
