9fa25a1e6b88bab3f1ebc4b8b353fe8cbcd42271
odong2 <mjey54@naver.com>
2022-11-01 02:47:48 +0900

    Feat: coupon 기능 추가

---
+++ src/main/java/com/finalpj/nbw/coupon/domain/Coupon.java
@@ -0,0 +1,14 @@
+package com.finalpj.nbw.coupon.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Coupon {
+    private Integer cp_no;
+    private String cp_name;
+    private String cp_price;
+}
