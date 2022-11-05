3ab89535d82b75d34d11dd47a3da176f5d92630b
odong2 <mjey54@naver.com>
2022-11-05 20:32:47 +0900

    Fix: 충돌로 주석 처리

--- src/main/java/com/finalpj/nbw/mypage/controller/MypageEventController.java
+++ src/main/java/com/finalpj/nbw/mypage/controller/MypageEventController.java
@@ -1,25 +1,25 @@
-package com.finalpj.nbw.mypage.controller;
-
-import com.finalpj.nbw.event.service.EventService;
-import lombok.extern.log4j.Log4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Log4j
-@RequestMapping("/mypage/*")
-@Controller
-public class MypageEventController {
-
-    EventService eventService;
-
-    public MypageEventController(EventService eventService) {
-        this.eventService = eventService;
-    }
-
-    @GetMapping("eventlist")
-    public String mypageEventList() {
-        return "mypage/event/event";
-    }
-
-}
+//package com.finalpj.nbw.mypage.controller;
+//
+//import com.finalpj.nbw.event.service.EventService;
+//import lombok.extern.log4j.Log4j;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Log4j
+//@RequestMapping("/mypage/*")
+//@Controller
+//public class MypageEventController {
+//
+//    EventService eventService;
+//
+//    public MypageEventController(EventService eventService) {
+//        this.eventService = eventService;
+//    }
+//
+//    @GetMapping("eventlist")
+//    public String mypageEventList() {
+//        return "mypage/event/event";
+//    }
+//
+//}
