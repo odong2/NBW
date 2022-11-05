860eea881ef0b2e879f1ce62bceee53c022f6660
odong2 <mjey54@naver.com>
2022-11-05 18:28:41 +0900

    Feat: 회원 결제 기능 완성

--- src/main/java/com/finalpj/nbw/payment/dao/PaymentDao.java
+++ src/main/java/com/finalpj/nbw/payment/dao/PaymentDao.java
@@ -1,13 +1,19 @@
 package com.finalpj.nbw.payment.dao;
 
+import com.finalpj.nbw.payment.domain.CartProduct;
 import com.finalpj.nbw.payment.domain.Payment;
 
-public interface PaymentDao {
-    // tb_memberpayment에 결제정보 넣기(1건)
-    int insertMemPayment(Payment paymentDto) throws Exception;
-    // tb_memberpaymentdetail 결제 정보 넣기(n건)
-    int insertMemPayDetail(Payment paymentDto) throws Exception;
-    // tb
+import java.util.List;
+import java.util.Map;
 
+public interface PaymentDao {
+    // tb_memberPayment에 결제정보 넣기(1건)
+    int insertMemPayment(Payment paymentDto) throws Exception;
+    // tb_memberPaymentdetail 결제 정보 넣기(n건)
+    int insertMemPayDetail(List<Map<String,Object>> payDetailList) throws Exception;
+
+    List<CartProduct> selectPayAfter (String order_no) throws Exception;
+
+    Map<String,Object> selectReceiver (String order_no) throws Exception;
 
 }
--- src/main/java/com/finalpj/nbw/payment/service/PaymentService.java
+++ src/main/java/com/finalpj/nbw/payment/service/PaymentService.java
@@ -2,10 +2,22 @@ package com.finalpj.nbw.payment.service;
 
 import com.finalpj.nbw.coupon.dao.CouponDao;
 import com.finalpj.nbw.coupon.domain.Coupon;
+import com.finalpj.nbw.member.dao.MemberDao;
+import com.finalpj.nbw.member.domain.Member;
 import com.finalpj.nbw.payment.dao.PaymentDao;
+import com.finalpj.nbw.payment.domain.CartProduct;
+import com.finalpj.nbw.payment.domain.PayDetail;
+import com.finalpj.nbw.payment.domain.Payment;
 import com.finalpj.nbw.product.dao.ProductDao;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Log4j
 @Service
@@ -14,14 +26,85 @@ public class PaymentService {
     private final PaymentDao paymentDao;
     private final CouponDao couponDao;
     private final ProductDao productDao;
+    private final MemberDao memberDao;
 
-    public PaymentService(PaymentDao paymentDao, CouponDao couponDao, ProductDao productDao){
+    public PaymentService(PaymentDao paymentDao, CouponDao couponDao, ProductDao productDao, MemberDao memberDao){
         this.paymentDao = paymentDao;
         this.couponDao = couponDao;
         this.productDao= productDao;
+        this.memberDao = memberDao;
+    }
+    /*************************  결제하기(회원)  ************************/
+    @Transactional(rollbackFor = Exception.class)
+    public String memorderPay(Payment paymentDto, HttpSession session) throws Exception{
+        String Msg = "";
+        String cp_no = paymentDto.getCp_no();
+        int used_point = paymentDto.getUsed_point();
+        int mem_point = paymentDto.getMem_point();
+        String mem_id = paymentDto.getMem_id();
+        Map<String,Object> pMap = null;
+        // 쿠폰이 '0'이면 null값으로 변환
+        if(cp_no == "0"){
+            paymentDto.setCp_no(null);
+        }
+        // (1) tb_mempayment 테이블에 삽입
+        paymentDao.insertMemPayment(paymentDto);
+        
+        // (2) 상품의 정보 데이터를 List 형태로 변환
+        List<Map<String,Object>> payDetailList = new ArrayList<>();
+        for(int i = 0; i < paymentDto.getP_no().length; i++){
+            pMap = new HashMap<String, Object>();
+
+            String order_no = (String)paymentDto.getOrder_no();
+            int p_no = (int)paymentDto.getP_no()[i];
+            int p_count = (int)paymentDto.getP_count()[i];
+
+            pMap.put("p_no", p_no);
+            pMap.put("p_count", p_count);
+            pMap.put("order_no", order_no);
+            payDetailList.add(pMap);
+        }
+        // (3) 결제 디테일 테이블에 다중 Insert
+        paymentDao.insertMemPayDetail(payDetailList);
+
+        // (4) 쿠폰 사용 하였을 경우 coupon_zip에서 쿠폰 삭제
+        if(cp_no != null){
+            pMap = new HashMap<>();
+            pMap.put("mem_id", mem_id);
+            pMap.put("cp_no", cp_no);
+            couponDao.deleteMemCoupon(pMap);
+        }
+        // (5) point 사용 하였을 경우 point 차감
+        if(used_point != 0){
+            pMap = new HashMap<>();
+            mem_point = mem_point - used_point;
+            pMap.put("mem_id", mem_id);
+            pMap.put("mem_point", mem_point);
+            // db에 mem_point 업데이트
+            memberDao.updateMemPoint(pMap);
+            // session의 mem_point 업데이트
+            Member member = (Member)session.getAttribute("member");
+            member.setMem_point(mem_point);
+            session.setAttribute("member", member);
+        }
+
+        // 주문한 상품의 재고 감소
+        
+        Msg = "PAY_OK";
+        return Msg;
+    }
+    /**************************** 결제 후 주문 상품 상세내역 조회 ************************************/
+    public List<CartProduct> afterPaySearchOrder(String order_no) throws Exception{
+        List<CartProduct> productList = null;
+        productList = paymentDao.selectPayAfter(order_no);
+        return productList;
     }
 
-
-
+    /**************************** 결제 후 수령인 상세내역 및 결제금액 조회  ************************************/
+    public Map<String, Object> afterpaySearchReceiver(String order_no) throws Exception{
+        Map<String, Object> rMap= null;
+        rMap = paymentDao.selectReceiver(order_no);
+        return rMap;
+    }
 
 }
