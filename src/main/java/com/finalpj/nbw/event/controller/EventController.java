65ec03ed4538a57c5637b0f0bbe7860140d9a2f6
dongcarry96 <fhakwpak6@naver.com>
2022-11-07 13:52:30 +0900

    Feat: 이벤트 컨트롤러 수정

--- src/main/java/com/finalpj/nbw/event/controller/EventController.java
+++ src/main/java/com/finalpj/nbw/event/controller/EventController.java
@@ -1,9 +1,8 @@
 package com.finalpj.nbw.event.controller;
+
 import com.finalpj.nbw.event.service.EventService;
 import com.finalpj.nbw.event.domain.Event;
 import lombok.extern.log4j.Log4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +18,9 @@ public class EventController {
     public EventController(EventService eventService){
         this.eventService = eventService;
     }
+
     /******************************* [[이벤트 리스트 페이지]] *****************************/
-    @GetMapping("/list")
+    @GetMapping("list")
     public String eventList(Model m) throws Exception {
         log.info("컨트롤러 이벤트 리스트 호출 성공");
         m.addAttribute("eventSelectAll", eventService.eventList());
@@ -28,11 +28,16 @@ public class EventController {
         return "/event/eventList";
     }
     /***************************** [[이벤트 디테일 페이지]] *******************************/
-    @GetMapping("/detail")
+    @GetMapping("detail")
     public String eventRead(Integer ev_no, Model m){
-        log.info(ev_no);
         try {
             Event event = (Event) eventService.eventRead(ev_no);
+            if(event.getEv_file() != null) {
+                String saveFileName = event.getEv_file();
+                int idx = saveFileName.indexOf("_");
+                String originalFileName = saveFileName.substring(idx+1);
+//                event.setEv_filename(originalFileName);
+            }
             m.addAttribute("eventSelect",event);
             log.info(event);
         } catch (Exception e) {
