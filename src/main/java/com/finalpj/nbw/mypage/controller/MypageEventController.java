08c3fd5995f0aec35022ed661746a0c1292cace0
dongcarry96 <fhakwpak6@naver.com>
2022-11-07 15:22:42 +0900

    Feat: 마이페이지 이벤트 컨트롤러 수정

--- src/main/java/com/finalpj/nbw/mypage/controller/MypageEventController.java
+++ src/main/java/com/finalpj/nbw/mypage/controller/MypageEventController.java
@@ -1,6 +1,8 @@
 package com.finalpj.nbw.mypage.controller;
 
+import com.finalpj.nbw.event.domain.Event;
 import com.finalpj.nbw.event.service.EventService;
+import com.finalpj.nbw.member.domain.Member;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Log4j
 @RequestMapping("/mypage/event/")
@@ -22,11 +25,16 @@ public class MypageEventController {
     /********************************* [[신청한 이벤트 페이지]] *********************************/
     @GetMapping("/list")
     public String mypageEventList(Model m, HttpSession session) throws Exception {
-
-
+        Member member = (Member) session.getAttribute("member");
+        List<Event> myEventSelectAll = null;
+        if(member != null) { // 회원일 경우
+            String id = member.getMem_id();
+            myEventSelectAll = eventService.mypageEventList(id);
+        }
         log.info("qna 컨트롤러 호출 성공");
         log.info("mypageEventList 마이페이지 컨트롤러 호출 성공 ");
-        m.addAttribute("myEventSelectAll",eventService.mypageEventList());
+        log.info("myEventSelectAll은 : "+myEventSelectAll);
+        m.addAttribute("myEventSelectAll",myEventSelectAll);
         return "mypage/event/event";
     }
 
