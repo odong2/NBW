8c252292557a8c54f6decf03b10c1198b87598c0
dongcarry96 <fhakwpak6@naver.com>
2022-11-07 15:23:38 +0900

    Feat: qna 다오 수정

--- src/main/java/com/finalpj/nbw/qna/dao/QnaDao.java
+++ src/main/java/com/finalpj/nbw/qna/dao/QnaDao.java
@@ -8,13 +8,13 @@ import java.util.List;
 public interface QnaDao {
 
     /****************************** Qna 전체 조회 ***************************/
-    List<Qna> qnaList() throws Exception;
+    List<Qna> qnaList(String id) throws Exception;
     /****************************** Qna 한건 조회 ***************************/
     Qna qnaRead(Integer qn_no) throws Exception;
     /****************************** Qna 처리중 조회 *****************************/
-    List<Qna> qnaIngList() throws Exception;
+    List<Qna> qnaIngList(String id) throws Exception;
     /****************************** Qna 답변완료 조회 *****************************/
-    List<Qna> qnaFinishList() throws Exception;
+    List<Qna> qnaFinishList(String id) throws Exception;
     /****************************** Qna 한건 삭제 ************************************/
     int qnaDelete(Integer qn_no) throws Exception;
     /****************************** Qna 전체 삭제 ************************/
