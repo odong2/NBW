052221c36bd0e3f74f38536c3f1a06a0cc3ce69a
dongcarry96 <fhakwpak6@naver.com>
2022-11-13 05:00:45 +0900

    Feat: 이벤트 수정

--- src/main/java/com/finalpj/nbw/event/domain/EventMember.java
+++ src/main/java/com/finalpj/nbw/event/domain/EventMember.java
@@ -11,12 +11,16 @@ public class EventMember {
     private Integer ev_no;
     private String mem_id;
     private String mem_name;
+    private String mem_phone;
     private String mem_gender;
     private String mem_birthday;
-    private String mem_phone;
+    private String mem_email;
+    private String mem_status;
+
 
     public EventMember (Integer ev_no, String mem_id, String mem_name,
-                        String mem_gender, String mem_birthday, String mem_phone
+                        String mem_gender, String mem_birthday, String mem_phone,
+                        String mem_email, String mem_status
                         ) {
         this.ev_no = ev_no;
         this.mem_id = mem_id;
@@ -24,6 +28,8 @@ public class EventMember {
         this.mem_gender = mem_gender;
         this.mem_birthday = mem_birthday;
         this.mem_phone = mem_phone;
+        this.mem_email = mem_email;
+        this.mem_status = mem_status;
     }
 
 }
